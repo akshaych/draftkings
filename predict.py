3584a0f58763936612faa86a45960a0902ae14f6
akshaych <akcm96@gmail.com>
2017-03-17 13:07:26 -0400

    predict.py fix

--- predict.py
+++ predict.py
@@ -5,17 +5,17 @@ import csv
 from operator import itemgetter
 from pymongo import MongoClient
 
-# init = initial.Initialize()
-# init.retrieve_url()
-# team_list, player_list = init.assign_teams()
-#
-# print team_list
+init = initial.Initialize()
+init.retrieve_url()
+team_list, player_list = init.assign_teams()
 
-#team_update = update.TeamUpdate(team_list)
-#team_update.update_teams()
-#
-# test2 = update.PlayerUpdate(player_list)
-# test2.update_player()
+print team_list
+
+team_update = update.TeamUpdate(team_list)
+team_update.update_teams()
+
+test2 = update.PlayerUpdate(player_list)
+test2.update_player()
 
 client = MongoClient()
 players = client['players']
